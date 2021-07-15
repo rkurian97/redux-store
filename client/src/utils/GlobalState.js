6056d63f69901aaa75a240cb7842dc958cd8416f
rkurian <kurian.rohit1229@gmail.com>
2021-07-14 20:28:18 -0500

    changed actions to typical object convention

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,23 +1,13 @@
-import React, { createContext, useContext } from "react";
+import {createStore} from 'redux'
 import { useProductReducer } from './reducers'
 
-const StoreContext = createContext();
-const { Provider } = StoreContext;
 
-const StoreProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useProductReducer({
-    products: [],
-    cart: [],
-    cartOpen: false,
-    categories: [],
-    currentCategory: '',
-  });
+const store= createStore(useProductReducer({
+  products: [],
+  cart: [],
+  cartOpen: false,
+  categories: [],
+  currentCategory: '',
+}));
 
-  return <Provider value={[state, dispatch]} {...props} />;
-};
-
-const useStoreContext = () => {
-  return useContext(StoreContext);
-};
-
-export { StoreProvider, useStoreContext };
+export default store
--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -1,11 +1,49 @@
 export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
 
-export const ADD_TO_CART = "ADD_TO_CART";
-export const ADD_MULTIPLE_TO_CART = "ADD_MULTIPLE_TO_CART";
-export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
-export const CLEAR_CART = "CLEAR_CART";
-export const UPDATE_CART_QUANTITY = "UPDATE_CART_QUANTITY";
-export const TOGGLE_CART = "TOGGLE_CART";
+export const ADD_TO_CART = () => {
+    return {
+        type: "ADD_TO_CART"
+    }
+};
 
-export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
-export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
+export const ADD_MULTIPLE_TO_CART= () => {
+    return {
+        type: "ADD_MULTIPLE_TO_CART"
+    }
+};
+
+export const REMOVE_FROM_CART= () => {
+    return {
+        type: "REMOVE_FROM_CART"
+    }
+}; 
+
+export const CLEAR_CART= () => {
+    return {
+        type: "CLEAR_CART"
+    }
+}; 
+
+export const UPDATE_CART_QUANTITY = () => {
+    return {
+        type: "UPDATE_CART_QUANTITY"
+    }
+}; 
+
+export const TOGGLE_CART = () => {
+    return {
+        type: "TOGGLE_CART"
+    }
+};
+
+export const UPDATE_CATEGORIES = () => {
+    return {
+        type: "UPDATE_CATEGORIES"
+    }
+}; 
+
+export const UPDATE_CURRENT_CATEGORY = () => {
+    return {
+        type: "UPDATE_CURRENT_CATEGORY"
+    }
+}; 
